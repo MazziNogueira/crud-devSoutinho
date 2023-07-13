bbe1a846fd0bed1383b8597405a22b0bd6afd79d
Marina Azzi Nogueira <mack@Macks-MBP.home>
2023-07-13 13:44:13 -0300

    aula 3.1 concluída: setup inicial com Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <>
+            <h1>Welcome to Next.js!!!</h1>
+            <p>This is the first page of our project ;)</p>
+        </>
+    )
+}
+
+export default HomePage
