9b97e3a7159fe5e80fe935c6cb8fd1544352b5b2
Marina Azzi Nogueira <mack@Macks-MacBook-Pro.local>
2023-07-12 22:13:34 -0300

    aula 2.2 concluída: READ

--- aula02/core/crud.ts
+++ aula02/core/crud.ts
@@ -1,19 +1,54 @@
 import fs from 'fs' //ES6
-
 // const fs = require('fs') => Common JS
 
 const DB_FILE_PATH = './aula02/core/db'
 
 console.log('[CRUD]')
 
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean
+}
+
 function create(content: string) {
+    const todo: Todo = {
+        //para chamar a data como string, usa-se o toISOString
+        date: new Date().toISOString(),
+        content: content,
+        done: false
+    }
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ]
+
     //salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content)
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+        dogs: []
+    }, null, 2))
     return content
 }
 
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8')
+    const db = JSON.parse(dbString || '{}')
 
+    // fail fast validation
+    if(!db.todos) {
+        return []
+    }
 
+    return db.todos
+}
 
+function CLEAR_DB(){
+    fs.writeFileSync(DB_FILE_PATH, '')
+}
 // [SIMULATION]
-console.log(create('Hoje eu tô estudando a aula 2.1!'))
+CLEAR_DB()
+create('Primeira TODO')
+create('Segunda TODO')
+console.log(read())
