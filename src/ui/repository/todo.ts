8331d25aabfa82024fd7c3718b97980c56f1b5d9
Marina Azzi Nogueira <mack@Macks-MBP.home>
2023-07-27 18:41:32 -0300

    parte 2 da aula 4.2 concluída: melhorando tipagem

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -17,12 +17,9 @@ function get({
         const todosString = await respostaDoServidor.text()
 
         // Como garantir a tipagem de tipos desconhecidos
-        const todosFromServer = JSON.parse(todosString).todos
-
-        // eslint-disable-next-line prettier/prettier, no-console
-        console.log('page', page)
-        // eslint-disable-next-line no-console
-        console.log('limit', limit)
+        const todosFromServer = parseTodosFromServer(
+            JSON.parse(todosString)
+        ).todos
 
         const ALL_TODOS = todosFromServer
         const startIndex = (page - 1) * limit
@@ -48,4 +45,39 @@ interface Todo {
     content: string
     date: Date
     done: boolean
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+    if (
+        responseBody !== null &&
+        typeof responseBody === 'object' &&
+        'todos' in responseBody &&
+        Array.isArray(responseBody.todos)
+    ) {
+        return {
+            todos: responseBody.todos.map((todo: unknown) => {
+                if (todo === null && typeof todo !== 'object') {
+                    throw new Error('Invalid todo from API')
+                }
+
+                const { id, content, date, done } = todo as {
+                    id: string
+                    content: string
+                    date: string
+                    done: string
+                }
+
+                return {
+                    id,
+                    content,
+                    done: String(done).toLowerCase() === 'true',
+                    date: new Date(date),
+                }
+            }),
+        }
+    }
+
+    return {
+        todos: [],
+    }
 }
